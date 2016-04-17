fa31f64f607c6515359dc54275c95176b730c85d
Zhipei Luo <zhipei.luo@yale.edu>
2016-04-17 00:00:54 -0400

    Some tweaks on Flashlight and slider

--- Assets/Scripts/LightControl.cs
+++ Assets/Scripts/LightControl.cs
@@ -12,11 +12,22 @@ public class LightControl : MonoBehaviour {
     private Light torch;
     private bool lightOn = false;
 	private bool batteryOut = false;
+    private Image sliderFill;
+    private Color deadFlashlight = new Color(110.0f / 255, 90.0f / 255, 80.0f / 255);
 
     // Use this for initialization
     void Start () {
         torch = GetComponent<Light>();
         torch.intensity = 0;
+        for(int obj = 0; obj < slider.transform.childCount; obj ++)
+        {
+            var tempObj = slider.transform.GetChild(obj).gameObject;
+            if(tempObj.name == "Fill Area")
+            {
+                tempObj = tempObj.transform.GetChild(0).gameObject;
+                sliderFill = tempObj.GetComponent<Image>();
+            }
+        }
     }
 
     IEnumerator turnOnLight()
@@ -32,6 +43,20 @@ public class LightControl : MonoBehaviour {
         }
         torch.intensity = flashCount;
     }
+
+    IEnumerator turnOffLight()
+    {
+        torch.intensity = 2;
+        yield return new WaitForSeconds(0.3f);
+        for (int i = flashCount; i > 0; i --)
+        {
+            torch.intensity = i;
+            yield return new WaitForSeconds(0.05f);
+            torch.intensity = 0;
+            yield return new WaitForSeconds(0.08f);
+        }
+        torch.intensity = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -41,7 +66,7 @@ public class LightControl : MonoBehaviour {
 					StartCoroutine (turnOnLight ());
 					lightOn = true;
 				} else {
-					torch.intensity = 0;
+                    StartCoroutine(turnOffLight());
 					lightOn = false;
 				}
 				nextTime = Time.time + stepTime;
@@ -50,12 +75,15 @@ public class LightControl : MonoBehaviour {
 
         if(lightOn)
         {
-            slider.value = Mathf.MoveTowards(slider.value, slider.value - 1.0f, 0.01f);
+            slider.value = Mathf.MoveTowards(slider.value, slider.value - 1.0f, 0.2f);
+            float remainBattery = slider.value / 100;
 			if (slider.value <= 0) {
 				batteryOut = true;
-				torch.intensity = 0;
-				lightOn = false;
+                StartCoroutine(turnOffLight());
+                lightOn = false;
 			}
+            torch.color = Color.Lerp(Color.white, deadFlashlight, 1 - remainBattery);
+            sliderFill.color = new Color(1, remainBattery, remainBattery, 1);
         }
 	}
 }
