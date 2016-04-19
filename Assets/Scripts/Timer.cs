3c868ab325a9dbea2a3982ce5af9dd719131cfa5
vverovvero <wwtchen@yahoo.com>
2016-04-18 21:26:07 -0400

    Imported 3D models, added timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public Text displayText;
+	public Text winText;
+
+	private bool gameOver = false;
+
+	//want six minutes (12AM - 6AM)
+	float countTime;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello world!");
+		winText.text = "";
+		countTime = 360;
+		setTime();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countTime > 0) {
+			countTime -= Time.deltaTime;
+		}
+		setTime();
+	}
+
+
+	// update the display of timer and win text
+	void setTime(){
+		displayText.text = "Seconds left: " + countTime.ToString();
+		if (countTime <= 0 && gameOver == false) {
+			//winning state
+			countTime = 0;
+			displayText.text = "Seconds left: " + countTime.ToString();
+			winText.text = "YOU SURVIVED THE NIGHT";
+		}
+	}
+
+	public void zeroTime(){
+		countTime = 0;
+		gameOver = true;
+		setTime ();
+	}
+
+}
---
+++ Assets/Scripts/playerLoseGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerLoseGame : MonoBehaviour {
+
+	public GameObject monsters;
+	public Text winText;
+	public Timer timer;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("game start, player alive");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		//Make player lose!
+		Debug.Log (other.gameObject.name);
+		if (other.gameObject.transform.parent.gameObject.name == monsters.name) {
+			//Debug.Log ("player loses");
+			winText.text = "GAME OVER";
+			timer.zeroTime ();
+		}
+	}
+
+}
