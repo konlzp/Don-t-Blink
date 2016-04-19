529f49f595ffd3791f7b4a103e42e23de9bc6272
Sean Niu <seanniu93@gmail.com>
2016-04-19 14:38:36 -0400

    Add rotation to face waypoint

--- Assets/Scripts/MonsterMover.cs
+++ Assets/Scripts/MonsterMover.cs
@@ -6,8 +6,9 @@ public class MonsterMover : MonoBehaviour
 {
 	public GameObject path;
 	public float speed;
+	private float rspeed = 10.0F;
 
-	private List<Transform> waypoints = new List<Transform> ();
+	private List<Transform> waypoints = new List<Transform> (); // List of waypoints
 	private int numWaypoints;
 	private int waypointIndex = 0;
 	private Transform nextWaypoint;
@@ -29,7 +30,15 @@ public class MonsterMover : MonoBehaviour
 		if (transform.position == nextWaypoint.position) { // Arrived at waypoint, need to move to next 
 			StartMoveToNextWaypoint ();
 		} else {
+			// Move object towards
 			transform.position = Vector3.MoveTowards (transform.position, nextWaypoint.position, speed);
+
+			// Face the waypoint
+			Vector3 targetDir = nextWaypoint.position - transform.position;
+			float step = rspeed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			Debug.DrawRay(transform.position, newDir, Color.red);
+			transform.rotation = Quaternion.LookRotation(newDir);
 		}
 	}
 
