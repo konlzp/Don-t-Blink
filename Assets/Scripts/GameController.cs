7944b08263ebcf68046eb633ed51653b79cf7a74
Zhipei Luo <zhipei.luo@yale.edu>
2016-04-28 16:22:08 -0400

    Add restart and quit
    'R' for restart, 'esc' for quit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Text uiText;
+    public Timer timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+
+    public void GameOver()
+    {
+        timer.zeroTime();
+        uiText.text = "You died";
+    }
+}
