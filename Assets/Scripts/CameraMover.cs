535271a6c70abc9717478a19784d94a2d3d3c48e
Sean Niu <seanniu93@gmail.com>
2016-05-02 20:16:34 -0400

    Rename CameraMoving to CameraMover and format

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update ()
+    {
+        yaw += speedH * Input.GetAxis ("Mouse X");
+        float newPitch = pitch - speedV * Input.GetAxis ("Mouse Y");
+        if (Mathf.Abs (newPitch) < 60) {
+            pitch = newPitch;
+        }
+        transform.eulerAngles = new Vector3 (pitch, yaw, 0.0f);
+    }
+}
